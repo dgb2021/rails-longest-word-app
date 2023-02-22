797b91706c1f4d8b24a5b202ef08c12f5eeefadf
Doug Barr <douglas.barr01@gmail.com>
2023-02-22 11:22:19 +0000

    results page displaying input params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,10 +4,15 @@ class GamesController < ApplicationController
     # display the new grid
     # display a new form
     @letters = generate_random
+   
   end
 
   def score
-    # 
+    # word cannot be built out of the original grid
+    # The word is valid according to the grid....but is not valid in ENGLISH
+    # The word is both valid accoding to the grid && is an English word
+     @user_answer = params[:word]
+
   end
 
   private
